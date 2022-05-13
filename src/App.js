eef3e9d9d00dd7811a3fb2a70f0b946be4e5a19d
Ketan Prasad <chetanprasad304@gmail.com>
2022-05-13 15:55:07 +0530

    added login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Component/Home/Home'
+import RoutesComp from './Component/RoutesComponent/RoutesComp'
+
+
+
 
 function App() {
   return (
     <div className="App">
+      <Home />
+      <RoutesComp />
+
+    
       
     </div>
   );
