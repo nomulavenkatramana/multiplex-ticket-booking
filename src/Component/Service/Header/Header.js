4afe0e496e49084755553fdcf46487e2cca8d22d
Amit Pathak <pathakamit764@gmail.com>
2022-05-13 11:52:27 +0530

    Adding movies branch

---
+++ src/Component/Service/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+function Header(){
+    return (
+        <nav>
+            <div>
+                <h1>Movies Details</h1>
+            </div>
+        </nav>
+    )
+}
+export default Header
