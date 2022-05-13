10089a279f88b5996f3512507b8f8afcbe9640a9
nomulavenkatramana <87078767+nomulavenkatramana@users.noreply.github.com>
2022-05-13 12:17:12 +0530

    adding ernings page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import EarningReport from './Components/EarningReport/EarningReport'
+import Searchdata from './Components/SearchData/SearchData';
 
 function App() {
   return (
     <div className="App">
-      
+      <EarningReport />
+      <Searchdata />
     </div>
   );
 }
