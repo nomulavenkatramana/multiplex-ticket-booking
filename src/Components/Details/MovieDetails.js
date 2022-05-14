135568222fa7af63d13a7eb607b4f7d4acbc2ee3
PranavKumbhar <pranavkumbhar7@gmail.com>
2022-05-14 11:09:33 +0530

    added movie details

---
+++ src/Components/Details/MovieDetails.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+const MovieDetails = () => { 
+  
+  function Strange() {
+    <div className='col-md-4'>
+      <div className="card p-4">
+        <div className="card-body">
+          <h6>Cast:Benedict Cumberbatch, Elizabeth Olsen, Chiwetel Ejiofor,	Benedict Wong, Xochitl Gomez,	Rachel McAdams</h6>
+          <h6>English,Hindi,Kannada,Malayalam,Tamil,Telugu</h6>
+          <h6>2h 6m•Action,Adventure,Fantasy•UA•6 May, 2022</h6>
+          <link href="https://www.youtube.com/results?search_query=dr.strange+2+trailer"
+            class="btn btn-primary me-3">Movie trailer</link>
+        </div>
+
+      </div>
+    </div>;
+
+  }
+
+  // const logoutUser = () => {
+    
+  // };
+  
+  return ( 
+   {/* ... */}
+  );
+};
+
+export default MovieDetails;
---
+++ src/Components/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer(){
+  return(
+    <div className="row">
+      <button className="btn btn-danger col-2"></button>
+      <div className="col-8 bg-dark"></div>
+      <button className="btn btn-primary col-2"></button>
+    </div>
+  );
+}
