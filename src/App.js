5fbe69af2380364fc47063a1eb04c1ec2676c501
Amit Pathak <pathakamit764@gmail.com>
2022-05-14 10:25:00 +0530

    pushing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import RoutesComp from './Component/RoutesComponent/RoutesComp'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Service/Header/Header';
 import Router from './Components/RouterComponent/Router'
+import AddMovies from './Component/Service/AddMovies';
+import EarningReport from './Components/EarningReport/EarningReport'
+import Searchdata from './Components/SearchData/SearchData';
+
 
 function App() {
   return (
